dd63ce1c294f8f2af0bec34044aef763aa99b1b7
ElMarcelFarias <marcel.leitefarias@gmail.com>
2025-05-18 14:49:59 -0300

    Change: Trabalhando com funcoes go de corroutinas, channels, etc

--- app/servidor-http.php
+++ app/servidor-http.php
@@ -1,12 +1,39 @@
 <?php
 
 use Swoole\Http\{Request, Response, Server};
+use Swoole\Coroutine\Http\Client;
 
 $servidor = new Server('0.0.0.0', 8080);
 
-$servidor->on('request', function ($request, $response) {    
-    $response->header('Content-Type', 'text/html; charset=utf8');
-    $response->end(print_r($request->header, true));
+// $servidor->on('request', function ($request, $response) {    
+//     $response->header('Content-Type', 'text/html; charset=utf8');
+//     $response->end(print_r($request->header, true));
+// });
+
+$servidor->on('request', function ($request, $response) {  
+
+    $channel = new chan(2);
+
+    go(function () use ($channel) {
+        $cliente = new Client('localhost', 8001);
+        $cliente->get('/app/servidor.php');
+
+        $conteudo = $cliente->getBody();
+        $channel->push($conteudo);
+    });
+
+    go(function () use ($channel) {
+        $conteudo = file_get_contents('arquivo.txt');
+        $channel->push($conteudo);
+    });
+
+    go(function () use ($channel, &$response) {
+        $primeiraResposta = $channel->pop();
+        $segundaResposta = $channel->pop();
+
+        $response->end($primeiraResposta . $segundaResposta);
+    });
 });
+
 
 $servidor->start();
